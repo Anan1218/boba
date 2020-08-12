206de5243eb161a410fc336ba49c8c3ae2ec5574
Mark Solomonik <marik.solomonik@gmail.com>
2020-08-12 13:00:39 -0700

    Added HomePage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import HomePage from "./pages/HomePage.js";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
